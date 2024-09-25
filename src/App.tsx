e4f6666927f77defa6f58783ad3d439884a497d6
Nelson Acosta <nelson.acosta.web@gmail.com>
2024-09-25 10:11:33 -0300

    refactor(app): integrate SearchForm and Movies components into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import Logo from './Logo/Logo'
-import { Search } from './Search/Search'
+import Logo from './logo/Logo'
+import { SearchForm } from './search/SearchForm'
 import { Helmet } from 'react-helmet';
+import { Movies } from './movie/Movies'
+import { useSearch } from './search/useSearch'
 import './App.css'
 
 function App() {
+  const { loading, movies, handleFormSubmit } = useSearch()
   return (
     <>
       <Helmet>
         <body className="bg-gray-50 dark:bg-gray-700" />
       </Helmet>
-      <header className='flex flex-col items-center gap-5 m-20'>
+      <header className='flex flex-col items-center gap-5 w-full py-8'>
         <div>
           <Logo />
         </div>
-        <Search />
+        <SearchForm handleFormSubmit={handleFormSubmit} />
       </header>
+      <main>
+        {
+          loading ? <p>cargando...</p> : <Movies movies={movies} />
+        }
+      </main>
     </>
   )
 }
